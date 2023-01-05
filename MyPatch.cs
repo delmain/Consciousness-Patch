fb9f8654b891be625e0efd1e6786123343e257c9
Vexthecollector <82162107+Vexthecollector@users.noreply.github.com>
2023-01-05 14:22:56 +0100

    Add project files.

---
+++ MyPatch.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using RimWorld;
+using HarmonyLib;
+
+namespace Custom_Rimworld_Mod
+{
+    public class MyPatch
+    {
+        public static void DoPatching()
+        {
+            var harmony = new Harmony("com.consciousness.patch");
+            
+            
+            harmony.PatchAll();
+        }
+
+    }
+}
