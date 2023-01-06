4a4ff54b2483bdbed6255614b984b57e7196d8f9
Vexthecollector <82162107+Vexthecollector@users.noreply.github.com>
2023-01-06 01:20:57 +0100

    Mod Settings

---
+++ Patches/ConsciousnessSettings.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using RimWorld;
+using HarmonyLib;
+using UnityEngine;
+
+namespace Consciousness_Patch.Patches
+{
+    public class ConsciousnessSettings : ModSettings
+    {
+        public ConsciousnessSettings()
+        {
+            ConsciousnessSettings.instance = this;
+        }
+        public static ConsciousnessSettings instance;
+
+        public override void ExposeData()
+        {
+            Scribe_Values.Look<bool>(ref this.setCap, "setCap");
+            Scribe_Values.Look<float>(ref this.maxCap, "maxCap",0.3f);
+            Scribe_Values.Look<float>(ref this.percentageModifierBP, "percentageModifier", 0.2f);
+            Scribe_Values.Look<float>(ref this.percentageModifierBR, "percentageModifier", 0.2f);
+            Scribe_Values.Look<float>(ref this.percentageModifierBF, "percentageModifier", 0.1f);
+            base.ExposeData();
+        }
+        public bool setCap;
+        public float mCMultiplier=1000f;
+        public float maxCap = 0.3f;
+        public float pMMultiplier = 100f;
+        public float percentageModifierBP = 0.2f;
+        public float percentageModifierBR = 0.2f;
+        public float percentageModifierBF = 0.1f;
+
+
+    }
+
+    public class ConsciousnessMod : Mod
+    {
+        public static ConsciousnessSettings settings;
+
+        public ConsciousnessMod(ModContentPack content) : base(content)
+        {
+            ConsciousnessMod.settings = this.GetSettings<ConsciousnessSettings>();
+        }
+
+        public override void DoSettingsWindowContents(Rect inRect)
+        {
+
+            Listing_Standard listingStandard = new Listing_Standard();
+            listingStandard.Begin(inRect);
+            listingStandard.Label("-----Placeholder Settings-----");
+            listingStandard.CheckboxLabeled("Has Cap", ref settings.setCap, "Sets if there is a maximum cap for Blood Filtration, Blood Pumping and Breathing scaling.");
+            listingStandard.Label("Maximum Cap: " +Math.Round(settings.maxCap*settings.mCMultiplier)+"%",-1, "The maximum cap until the stats no longer give any increases to Consciousness");
+            settings.maxCap = listingStandard.Slider(settings.maxCap, 0.1f, 1f);
+            listingStandard.Label("Blood Pumping Percentage Modifier: " + Math.Round(settings.percentageModifierBP*settings.pMMultiplier)+"%", -1, "How much each stat increases Consciousness." +
+                "\n20% means that 150% Blood Pumping increases Consciousness by 10% (20% default)");
+            settings.percentageModifierBP = listingStandard.Slider(settings.percentageModifierBP, 0.2f, 1f);
+            settings.percentageModifierBF = listingStandard.Slider(settings.percentageModifierBF, 0.1f, 1f);
+            listingStandard.Label("Breathing Percentage Modifier: " + Math.Round(settings.percentageModifierBR * settings.pMMultiplier) + "%", -1, "How much each stat increases Consciousness." +
+                "\n20% means that 150% Blood Pumping increases Consciousness by 10% (20% default)");
+            settings.percentageModifierBR = listingStandard.Slider(settings.percentageModifierBR, 0.2f, 1f);
+            listingStandard.Label("Blood Filtration Percentage Modifier: " + Math.Round(settings.percentageModifierBF * settings.pMMultiplier) + "%", -1, "How much each stat increases Consciousness." +
+                "\n20% means that 150% Blood Pumping increases Consciousness by 10%. (10% default)");
+            listingStandard.End();
+            base.DoSettingsWindowContents(inRect);
+            //ConsciousnessMod.settings.Write();
+        }
+
+        public override string SettingsCategory()
+        {
+            return "Consciousness Patch";
+        }
+    }
+
+
+
+
+
+
+}
--- Patches/TranspilerConsciousnessPatch.cs
+++ Patches/TranspilerConsciousnessPatch.cs
@@ -18,34 +18,108 @@ namespace Consciousness_Patch
     {
         static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
         {
-
             var codes = new List<CodeInstruction>(instructions);
-            List<int> startIndexes=new List<int>();
-            List<int> endIndexes = new List<int>();
+            int[] BloodPumping = getIndexes(codes);
+            int[] Breathing = getIndexes(codes, BloodPumping[0]);
+            int[] BloodFiltration = getIndexes(codes, Breathing[0]);
+            List<int[]> allVariables = new List<int[]>();
+            allVariables.Add(BloodFiltration);
+            allVariables.Add(BloodPumping);
+            allVariables.Add(Breathing);
+            if (Patches.ConsciousnessSettings.instance.setCap)
+            {
+                allVariables.ForEach(x => changeCap(ref codes, x));
+            }
+            else
+            {
+                removeLimit(ref codes, BloodFiltration);
+                getAllIndexes(ref codes, ref BloodPumping, ref Breathing, ref BloodFiltration);
+                removeLimit(ref codes, Breathing);
+                getAllIndexes(ref codes, ref BloodPumping, ref Breathing, ref BloodFiltration);
+                removeLimit(ref codes, BloodPumping);
+                getAllIndexes(ref codes, ref BloodPumping, ref Breathing, ref BloodFiltration);
+            }
+
+            changePercentage(ref codes, BloodPumping, Patches.ConsciousnessSettings.instance.percentageModifierBP);
+            changePercentage(ref codes, Breathing, Patches.ConsciousnessSettings.instance.percentageModifierBR);
+            changePercentage(ref codes, BloodFiltration, Patches.ConsciousnessSettings.instance.percentageModifierBF);
+
+            getAllIndexes(ref codes,ref BloodPumping,ref Breathing,ref BloodFiltration);
+
+
+            return codes.AsEnumerable();
+
+        }
+
+        public static void changePercentage(ref List<CodeInstruction> __codes, int[] ItemRange,float percentage)
+        {
+            for (var i = ItemRange[0]; i < ItemRange[1]; i++)
+            {
+                if (__codes[i].opcode == OpCodes.Ldc_R4 && (float)__codes[i].operand != 1)
+                {
+                    __codes[i].opcode = OpCodes.Nop;
+                    __codes.RemoveAt(i);
+                    __codes.Insert(i, new CodeInstruction(OpCodes.Ldc_R4, percentage));
+                }
+            }
+        }
+
+        public static void changeCap(ref List<CodeInstruction> __codes, int[] ItemRange)
+        {
+            float maxCap = Patches.ConsciousnessSettings.instance.maxCap * Patches.ConsciousnessSettings.instance.mCMultiplier / 100;
+            for (var i = ItemRange[0]; i < ItemRange[1]; i++)
+            {
+                if (__codes[i].opcode == OpCodes.Ldc_R4 && (float)__codes[i].operand==1)
+                {
+                    __codes[i].opcode = OpCodes.Nop;
+                    __codes.RemoveAt(i);
+                    __codes.Insert(i, new CodeInstruction(OpCodes.Ldc_R4, maxCap));
+                }
+            }
+        }
+
+        public static void getAllIndexes(ref List<CodeInstruction> __codes, ref int[] __BloodPumping, ref int[] __Breathing, ref int[] __BloodFiltration)
+        {
+            __BloodPumping = getIndexes(__codes);
+            __Breathing = getIndexes(__codes, __BloodPumping[0]);
+            __BloodFiltration = getIndexes(__codes, __Breathing[0]);
+        }
+
+
+        public static void removeLimit(ref List<CodeInstruction> __codes,int[] ItemRange)
+        {
+            for (var i = ItemRange[0]; i < ItemRange[1]; i++)
+            {
+                if (__codes[i].opcode == OpCodes.Ldc_R4)
+                {
+                    if ((float)__codes[i].operand == 1 && __codes[i + 1].opcode == OpCodes.Call)
+                    {
+                        __codes[i].opcode = OpCodes.Nop;
+                        __codes.RemoveRange(i + 1, 1);
+                    }
+                }
+            }
+            
+
+        }
+
+        public static int[] getIndexes(List<CodeInstruction> codes,int startIndex=-1)
+        {
+            int endIndex = -1;
 
             for (var i = 0; i < codes.Count; i++)
             {
-                if (codes[i].opcode == OpCodes.Ldc_R4)
+                if (codes[i].opcode == OpCodes.Ldsfld && startIndex == -1)
                 {
-                    if((float)codes[i].operand == 1 && codes[i + 1].opcode == OpCodes.Call){
-                        startIndexes.Add(i);
-                        endIndexes.Add(i+1);
-                    }
-                } 
-            }
-            for(var i = startIndexes.Count-1; i >= 0; i--)
-            {
-                codes[startIndexes[i]].opcode = OpCodes.Nop;
-                codes.RemoveRange(startIndexes[i] + 1, endIndexes[i] - startIndexes[i]);
-            }
-            /*
-                if (startIndexMath1 > -1 && endIndexMath1 > -1)
+                    startIndex = i;   
+                }
+                else if(codes[i].opcode== OpCodes.Ldsfld)
                 {
-                    codes[startIndexMath1].opcode = OpCodes.Nop;
-                    codes.RemoveRange(startIndexMath1+1, endIndexMath1- startIndexMath1);
-                }*/
-            return codes.AsEnumerable();
+                    endIndex = i;
+                }
+            }
 
+            return new int[] { startIndex, endIndex };
         }
     }
 }
