212a43c5d34dc86573607da78aed5f4c50715868
Vexthecollector <82162107+Vexthecollector@users.noreply.github.com>
2023-01-11 17:34:55 +0100

    Comments added

--- Patches/ConsciousnessSettings.cs
+++ Patches/ConsciousnessSettings.cs
@@ -28,8 +28,10 @@ namespace Consciousness_Patch.Patches
             base.ExposeData();
         }
         public bool setCap;
+        //Multiplier to display the stats properly to the user
         public float mCMultiplier=1000f;
         public float maxCap = 0.3f;
+        //Multiplier to display the stats properly to the user
         public float pMMultiplier = 100f;
         public float percentageModifierBP = 0.2f;
         public float percentageModifierBR = 0.2f;
@@ -47,23 +49,24 @@ namespace Consciousness_Patch.Patches
             ConsciousnessMod.settings = this.GetSettings<ConsciousnessSettings>();
         }
 
+        //Creates all the settings for the mod. 
         public override void DoSettingsWindowContents(Rect inRect)
         {
 
             Listing_Standard listingStandard = new Listing_Standard();
             listingStandard.Begin(inRect);
-            listingStandard.Label("-----Placeholder Settings-----");
+            listingStandard.Label("-----Settings-----");
             listingStandard.CheckboxLabeled("Has Cap", ref settings.setCap, "Sets if there is a maximum cap for Blood Filtration, Blood Pumping and Breathing scaling.");
             listingStandard.Label("Maximum Cap: " +Math.Round(settings.maxCap*settings.mCMultiplier)+"%",-1, "The maximum cap until the stats no longer give any increases to Consciousness");
             settings.maxCap = listingStandard.Slider(settings.maxCap, 0.1f, 1f);
             listingStandard.Label("Blood Pumping Percentage Modifier: " + Math.Round(settings.percentageModifierBP*settings.pMMultiplier)+"%", -1, "How much each stat increases Consciousness." +
-                "\n20% means that 150% Blood Pumping increases Consciousness by 10% (20% default)");
+                "\n20% means that 150% Blood Pumping increases Consciousness by 10% \n(20% default)");
             settings.percentageModifierBP = listingStandard.Slider(settings.percentageModifierBP, 0.1f, 1f);
             listingStandard.Label("Blood Filtration Percentage Modifier: " + Math.Round(settings.percentageModifierBF * settings.pMMultiplier) + "%", -1, "How much each stat increases Consciousness." +
-                "\n20% means that 150% Blood Pumping increases Consciousness by 10%. (10% default)");
+                "\n20% means that 150% Blood Filtration increases Consciousness by 10%. \n(10% default)");
             settings.percentageModifierBF = listingStandard.Slider(settings.percentageModifierBF, 0.1f, 1f);
             listingStandard.Label("Breathing Percentage Modifier: " + Math.Round(settings.percentageModifierBR * settings.pMMultiplier) + "%", -1, "How much each stat increases Consciousness." +
-                "\n20% means that 150% Blood Pumping increases Consciousness by 10% (20% default)");
+                "\n20% means that 150% Breathing increases Consciousness by 10% \n(20% default)");
             settings.percentageModifierBR = listingStandard.Slider(settings.percentageModifierBR, 0.1f, 1f);
             listingStandard.End();
             base.DoSettingsWindowContents(inRect);
--- Patches/TranspilerConsciousnessPatch.cs
+++ Patches/TranspilerConsciousnessPatch.cs
@@ -20,49 +20,52 @@ namespace Consciousness_Patch
         {
             var codes = new List<CodeInstruction>(instructions);
 
+            //Creates a Dictionary to easier change each setting.
             Dictionary<String,Dictionary<String,int>> mainMap = new Dictionary<String,Dictionary<String, int>>();
             AddMaptoMain(ref mainMap,"BloodPumping");
             AddMaptoMain(ref mainMap, "Breathing");
             AddMaptoMain(ref mainMap, "BloodFiltration");
 
+            //The map indexes for all three maps are gathered and stored
             mainMap["BloodPumping"]=getMapIndexes(mainMap["BloodPumping"], codes);
             mainMap["Breathing"] = getMapIndexes(mainMap["Breathing"], codes,mainMap["BloodPumping"]["Percentage"]);
             mainMap["BloodFiltration"] = getMapIndexes(mainMap["BloodFiltration"], codes, mainMap["Breathing"]["Percentage"]);
 
-            /*
-            Log.Message("BloodPumping: "+" Percentage: "+ mainMap["BloodPumping"]["Percentage"]+
-                " Cap: "+ mainMap["BloodPumping"]["Cap"]+" MathfMin: "+ mainMap["BloodPumping"]["MathfMin"]);
-            Log.Message("Breathing: " + " Percentage: " + mainMap["Breathing"]["Percentage"] +
-                " Cap: " + mainMap["Breathing"]["Cap"] + " MathfMin: " + mainMap["Breathing"]["MathfMin"]);
-            Log.Message("BloodFiltration: " + " Percentage: " + mainMap["BloodFiltration"]["Percentage"] +
-                " Cap: " + mainMap["BloodFiltration"]["Cap"] + " MathfMin: " + mainMap["BloodFiltration"]["MathfMin"]);
-            */
+            //OpCodes.Nop are set for each location.
+            codes[mainMap["BloodFiltration"]["Cap"]].opcode = OpCodes.Nop;
+            codes[mainMap["Breathing"]["Cap"]].opcode = OpCodes.Nop;
+            codes[mainMap["BloodPumping"]["Cap"]].opcode = OpCodes.Nop;
 
-            //changePercentages(ref codes, mainMap["BloodFiltration"]["Percentage"], Patches.ConsciousnessSettings.instance.percentageModifierBF);
-            //changePercentages(ref codes, mainMap["Breathing"]["Percentage"], Patches.ConsciousnessSettings.instance.percentageModifierBR);
-            //changePercentages(ref codes, mainMap["BloodPumping"]["Percentage"], Patches.ConsciousnessSettings.instance.percentageModifierBP);
-
+            //Percentages are changed for each stat.
+            ChangePercentage(ref codes, mainMap["BloodFiltration"]["Percentage"], Patches.ConsciousnessSettings.instance.percentageModifierBF);
+            ChangePercentage(ref codes, mainMap["Breathing"]["Percentage"], Patches.ConsciousnessSettings.instance.percentageModifierBR);
+            ChangePercentage(ref codes, mainMap["BloodPumping"]["Percentage"], Patches.ConsciousnessSettings.instance.percentageModifierBP);
+            
+            //If the user enables a cap, the cap will be changed to what the user set it to.
+            //Otherwise the cap will be removed.
             if (Patches.ConsciousnessSettings.instance.setCap)
             {
                 float maxCap = Patches.ConsciousnessSettings.instance.maxCap * Patches.ConsciousnessSettings.instance.mCMultiplier / 100;
-                changeCaps(ref codes, mainMap["BloodFiltration"]["Cap"], maxCap);
-                changeCaps(ref codes, mainMap["Breathing"]["Cap"], maxCap);
-                changeCaps(ref codes, mainMap["BloodPumping"]["Cap"], maxCap);
+                ChangeCap(ref codes, mainMap["BloodFiltration"]["Cap"], maxCap);
+                ChangeCap(ref codes, mainMap["Breathing"]["Cap"], maxCap);
+                ChangeCap(ref codes, mainMap["BloodPumping"]["Cap"], maxCap);
             }
             else
             {
 
-                removeLimits(ref codes, mainMap["BloodFiltration"]["Cap"], mainMap["BloodFiltration"]["MathfMin"]);
-                removeLimits(ref codes, mainMap["Breathing"]["Cap"], mainMap["Breathing"]["MathfMin"]);
-                removeLimits(ref codes, mainMap["BloodPumping"]["Cap"], mainMap["BloodPumping"]["MathfMin"]);
+                RemoveLimit(ref codes, mainMap["BloodFiltration"]["Cap"], mainMap["BloodFiltration"]["MathfMin"]);
+                RemoveLimit(ref codes, mainMap["Breathing"]["Cap"], mainMap["Breathing"]["MathfMin"]);
+                RemoveLimit(ref codes, mainMap["BloodPumping"]["Cap"], mainMap["BloodPumping"]["MathfMin"]);
 
 
             }
-
+            
+            Log.Message("Consciousness has been Patched");
             return codes.AsEnumerable();
 
         }
 
+        //Creates a basic map with 0 indexes to manage it afterwards.
         public static void AddMaptoMain(ref Dictionary<String, Dictionary<String, int>> __mainMap, String name)
         {
             Dictionary<String, int> subMap = new Dictionary<String, int>();
@@ -73,6 +76,10 @@ namespace Consciousness_Patch
         }
 
 
+        //Gets the indexes for each of the required things to change.
+        //Cap is the location that determines how high each stat can go, before it gives no benefit anymore.
+        //Mathfmin is the location of the Mathf.Min function, which sets the limit for each stat.
+        //Percentage is the location of how much each stat scales.
         public static Dictionary<String, int> getMapIndexes(Dictionary<String, int> subMap, List<CodeInstruction> codes,int start=0)
         {
             for (var i = start; i < codes.Count; i++)
@@ -83,7 +90,7 @@ namespace Consciousness_Patch
                     {
                         subMap["Cap"] = i;
                         subMap["MathfMin"] = i+1;
-                            subMap["Percentage"] = i + 3;
+                        subMap["Percentage"] = i + 3;
                         break;
                     }
 
@@ -92,22 +99,23 @@ namespace Consciousness_Patch
             return subMap;
         }
 
-        public static void removeLimits(ref List<CodeInstruction> __codes,int cap,int mathfMin)
+        //Removes the limit by deleting the location of the Cap and MathfMin 
+        public static void RemoveLimit(ref List<CodeInstruction> __codes,int cap,int mathfMin)
         {
-            __codes[cap].opcode = OpCodes.Nop;
             __codes.RemoveRange(cap+1, 1);
         }
 
-        public static void changePercentages(ref List<CodeInstruction> __codes,int percentageIndex,float percentage)
+        //Changes the percentage to a new value. 1f means every point over 100% increases consciousness by 1.
+        public static void ChangePercentage(ref List<CodeInstruction> __codes,int percentageIndex,float percentage)
         {
-            __codes[percentageIndex].opcode = OpCodes.Nop;
-            __codes.RemoveAt(percentageIndex+1);
-            __codes.Insert(percentageIndex+1, new CodeInstruction(OpCodes.Ldc_R4, percentage));
+            __codes.RemoveAt(percentageIndex);
+            __codes.Insert(percentageIndex, new CodeInstruction(OpCodes.Ldc_R4, percentage));
         }
 
-        public static void changeCaps(ref List<CodeInstruction> __codes,int capIndex, float maxCap)
+        //Sets a new cap. This cap applies to each stat it is set for. 1f would mean that it behaves like vanilla.
+        //2f means it scales up to 200%, before cutting of.
+        public static void ChangeCap(ref List<CodeInstruction> __codes,int capIndex, float maxCap)
         {
-            __codes[capIndex].opcode = OpCodes.Nop;
             __codes.RemoveAt(capIndex+1);
             __codes.Insert(capIndex+1, new CodeInstruction(OpCodes.Ldc_R4, maxCap));
         }
